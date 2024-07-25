b1da14fd2623fb80cfa4fde8e6d2aa04760e5fc5
LeislaCV <utm23090765@utma.edu.mx>
2024-07-25 13:50:13 -0600

    25/07/2024

---
+++ api/class/CardClass.js
@@ -0,0 +1,60 @@
+//Crear una tarjeta
+//Obtener una tarjeta
+//Obtener tarjetas
+ import CardModel from "../models/CardModel.js";
+class ManagerCard{
+    constructor(
+        userId,
+        accountId,
+        cardNumber,
+        cardType,
+        expirationDate,
+        securityCode,
+        status
+    ){
+        this.userId = userId;
+        this.accountId =accountId;
+        this.cardNumber = cardNumber;
+        this.cardType = cardType;
+        this.expiration = expirationDate;
+        this.securityCode = securityCode;
+        this.status = status;
+        }
+
+        async createCard(){
+            try {
+                await CardModel.create({
+                userId: this.userId, 
+                accountId: this.accountId,
+                cardNumber: this.cardNumber, 
+                cardType: this.cardType, 
+                expirationDate: this.expiration, 
+                securityCode: this.securityCode, 
+                status: this.status 
+                });
+                return "Ok";
+            } catch (error) {
+                throw new Error(`Error al cargar la tarjeta: ${error}`);
+            }
+        }
+        async getCards(){
+            try {
+                const cards = await CardModel.find();
+                return cards;
+            } catch (error) {
+                throw new Error(`Error al obtener tarjeta: ${error}`);
+    
+            }
+        }
+    
+        async getCard(id){
+            try {
+                const card = await CardModel.findById(id);
+                return card;
+            } catch (error) {
+                throw new Error(`Error al obtener tarjeta: ${error}`);
+    
+            }
+        }
+    
+    }
